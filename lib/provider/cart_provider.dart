d6d3df59558f4995111b978db09f62c71105b172
khushjeet <khushjeetkumar100@gmail.com>
2023-09-20 08:48:18 +0530

    adding  Stat Notifier Provider

--- lib/provider/cart_provider.dart
+++ lib/provider/cart_provider.dart
@@ -1,6 +1,10 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:uber_shop/models/car_model.dart';
 
+final cartProvider =
+    StateNotifierProvider<CartNotifier, Map<String, CartModels>>(
+        (ref) => CartNotifier());
+
 class CartNotifier extends StateNotifier<Map<String, CartModels>> {
   CartNotifier() : super({});
 
@@ -19,14 +23,29 @@ class CartNotifier extends StateNotifier<Map<String, CartModels>> {
       state = {
         ...state,
         productId: CartModels(
-            productName: state[productId]!.productName,
-            productId: state[productId]!.productId,
-            imageUrl: state[productId]!.imageUrl,
-            price: state[productId]!.price,
-            vendorId: state[productId]!.vendorId,
-            productSize: state[productId]!.productSize,
-            productQuantity: state[productId]!.productQuantity,
-            quantity: state[productId]!.productQuantity)
+          productName: state[productId]!.productName,
+          productId: state[productId]!.productId,
+          imageUrl: state[productId]!.imageUrl,
+          price: state[productId]!.price,
+          vendorId: state[productId]!.vendorId,
+          productSize: state[productId]!.productSize,
+          productQuantity: state[productId]!.productQuantity,
+          quantity: state[productId]!.quantity + 1,
+        )
+      };
+    } else {
+      state = {
+        ...state,
+        productId: CartModels(
+          productName: productName,
+          productId: productId,
+          imageUrl: imageUrl,
+          price: price,
+          vendorId: vendorId,
+          productSize: productSize,
+          productQuantity: productQuantity,
+          quantity: quantity,
+        )
       };
     }
   }
